7b03ed10dca6846c7935281c85b37fe4b79cf757
horacio barrios <horacioxbarrios@gmail.com>
2023-09-25 19:37:01 -0300

    Lo visto en la primerca clase

--- clase1_ConsoleApp/ConsoleAppClase1/Program.cs
+++ clase1_ConsoleApp/ConsoleAppClase1/Program.cs
@@ -4,7 +4,47 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(  "Hola Mundo, Hola cSharp");
+            //comentario de una linea
+            /*comentario multilinea 
+             * 
+             * cSharp es un lenguaje de fuerte tipado
+             * 
+             * Las sentencias terminan con ';' punto y coma
+             * 
+             * Se usan llaves '{}" para bloques de codigo
+             * 
+             * se usa parentesis '()' para estructuras (if, while, for, etc)
+             * 
+             * El scope de las variables cambia con lo que vimos en Python
+             * 
+             * 
+             */
+
+            int x = 1;
+            if (x == 1)
+            {
+                int a = 2;
+                Console.WriteLine("'Dentro del 1er if' x == 1");
+                if(a == 2)
+                {
+                    Console.WriteLine("'Dentro del 2do if' a == 2");
+                }
+            }
+
+            //Console.WriteLine($"en este scope la variable a no existe{a}"); //use interpolacion de string, para ver el valor de 'a'
+            Console.WriteLine("en este scope la variable a no existe");
+
+            while(x < 10)
+            {
+                Console.WriteLine( "PreIncremento '1' " + x); //1
+                x++;
+                Console.WriteLine("PreIncremento '2' " + x);//2
+                ++x;
+                Console.WriteLine("PosIncremento '1' " + x);//3 *tambien le suma uno, pero la dif es que primero le suma y despues lo muestra.
+
+            }
+
         }
     }
 }
